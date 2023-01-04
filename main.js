60e9b19b71d1f56bf93ae6506d6fe11d28b8bbe1
benr0th <53961190+benr0th@users.noreply.github.com>
2023-01-04 10:11:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+function hideOptions() {
+  hideElement("options");
+  showElement("back-button", "block");
+}
+
+function hideElement(element) {
+  document.getElementById(element).style.display = "none";
+}
+
+function showElement(element, style) {
+  document.getElementById(element).style.display = style;
+}
+
+function optionSelect(option) {
+  if (option === "start") {
+    hideElement("start-button");
+    hideElement("press-start");
+    showElement("options", "block");
+  }
+  else if (option === "back") {
+    showElement("options", "block");
+    hideElement("back-button");
+    hideElement("editing");
+    hideElement("gameDev");
+    hideElement("programming");
+  }
+  else if (option === "editing") {
+    hideOptions();
+    showElement("editing", "block");
+  }
+  else if (option === "gameDev") {
+    hideOptions();
+    showElement("gameDev", "block");
+    showElement("box", "inline-block");
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  var selectedOption = document.getElementsByClassName("selected")[0];
+  if (document.getElementById("start-button").style.display === "none") {
+    if (event.code === "ArrowDown") {
+      var nextOption = selectedOption.nextElementSibling;
+      if (nextOption) {
+        selectedOption.classList.remove("selected");
+        nextOption.classList.add("selected");
+      }
+    } else if (event.code === "ArrowUp") {
+      var prevOption = selectedOption.previousElementSibling;
+      if (prevOption) {
+        selectedOption.classList.remove("selected");
+        prevOption.classList.add("selected");
+      }
+    }
+    if (event.code === "Enter") {
+      document.getElementsByClassName("selected")[0].click();
+    }
+  }
+});
+
+var options = document.getElementsByClassName("option");
+for (var i = 0; i < options.length; i++) {
+  options[i].addEventListener("mouseenter", function() {
+    var selectedOption = document.getElementsByClassName("selected")[0];
+    selectedOption.classList.remove("selected");
+    this.classList.add("selected");
+  });
+}
