ac0de6a2529ee32e38a2183272493167d2b9c9a8
benr0th <53961190+benr0th@users.noreply.github.com>
2023-01-12 21:24:36 -0500

    Put js functions in html file

--- src/main.js
+++ src/main.js
@@ -1,27 +1,4 @@
 
-
-document.addEventListener("keydown", function(event) {
-  var selectedOption = document.getElementsByClassName("selected")[0];
-  if (document.getElementById("start-button").style.display === "none") {
-    if (event.code === "ArrowDown") {
-      var nextOption = selectedOption.nextElementSibling;
-      if (nextOption) {
-        selectedOption.classList.remove("selected");
-        nextOption.classList.add("selected");
-      }
-    } else if (event.code === "ArrowUp") {
-      var prevOption = selectedOption.previousElementSibling;
-      if (prevOption) {
-        selectedOption.classList.remove("selected");
-        prevOption.classList.add("selected");
-      }
-    }
-    if (event.code === "Enter") {
-      document.getElementsByClassName("selected")[0].click();
-    }
-  }
-});
-
 var options = document.getElementsByClassName("option");
 for (var i = 0; i < options.length; i++) {
   options[i].addEventListener("mouseenter", function() {
@@ -32,7 +9,7 @@ for (var i = 0; i < options.length; i++) {
 }
 
 let typed = new Typed('#bio', {
-  strings: ['Hi there my name is Ben I like to code and learn things^1000 <br> The things I learn are pretty cool sometimes\n'],
+  strings: [''],
   typeSpeed: 20,
   loop: false
 });
