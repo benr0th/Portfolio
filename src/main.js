8905ff0750b38d4b545b2a1f6642c870dc3fc07e
benr0th <53961190+benr0th@users.noreply.github.com>
2023-01-13 17:56:23 -0500

    Added bio sections

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+document.addEventListener("keydown", function(event) {
+  var selectedOption = document.getElementsByClassName("selected")[0];
+  if (document.getElementById("start-button").style.display === "none") {
+    if (event.code === "ArrowDown") {
+      var nextOption = selectedOption.nextElementSibling;
+      if (nextOption) {
+        selectedOption.classList.remove("selected");
+        nextOption.classList.add("selected");
+      }
+    } else if (event.code === "ArrowUp") {
+      var prevOption = selectedOption.previousElementSibling;
+      if (prevOption) {
+        selectedOption.classList.remove("selected");
+        prevOption.classList.add("selected");
+      }
+    }
+    if (event.code === "Enter") {
+      document.getElementsByClassName("selected")[0].click();
+    }
+  }
+});
 
 var options = document.getElementsByClassName("option");
 for (var i = 0; i < options.length; i++) {
@@ -7,9 +28,3 @@ for (var i = 0; i < options.length; i++) {
     this.classList.add("selected");
   });
 }
-
-let typed = new Typed('#bio', {
-  strings: [''],
-  typeSpeed: 20,
-  loop: false
-});
